d73952dcc959d66c20d59dfd426f87676174c18c
YusraAshar <yashar@csumb.edu>
2025-08-06 01:07:19 -0700

    final changes

--- index.mjs
+++ index.mjs
@@ -9,21 +9,16 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
-});
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-//environment variables in Render
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,        // e.g., u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
-    user: process.env.DB_USER,        // e.g., mp26jo8kppunxiix
-    password: process.env.DB_PASSWORD,// from Canvas
-    database: process.env.DB_NAME,    // jkvs50o7ha454ro4
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
 });
@@ -36,11 +31,12 @@ app.get("/dbTest", async (req, res) => {
     conn.release();
 });
 
-// Homepage
+// Home
 app.get("/", (req, res) => {
     res.send("Hello Express app!");
 });
 
-app.listen(port, () => {
-    console.log(`Express server running at http://localhost:${port}`);
+// Start server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
 });
